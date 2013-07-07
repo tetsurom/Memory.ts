ea5fc3beed052390fe2fcd0fdbaa6b5b67dfe11a
Tetsuro Matsumura <matsumura.t.lab@gmail.com>
2013-07-07 18:44:56 +0900

    add spectralnorm. closed #6

--- app.ts
+++ app.ts
@@ -293,6 +293,18 @@ class Pointer {
         typedArray[j++] = 0;
     }
 
+    InclInt32ValueOffset(Offset: number): number {
+        var pre = this.GetInt32ValueOffset(Offset);
+        this.SetInt32ValueOffset(Offset, pre + 1);
+        return pre;
+    }
+
+    InclInt32Value(): number {
+        var pre = this.GetInt32Value();
+        this.SetInt32Value(pre + 1);
+        return pre;
+    }
+
     Add(n: number): Pointer {
         return new Pointer(this.MemoryBlockRef, this.Offset + n);
     }
@@ -597,8 +609,222 @@ class Tester {
         main(2, args);
     }
 
+    //---------------------------------------------------------------------------------------
+    // fannkuchredux.c
+    //---------------------------------------------------------------------------------------
+
+    fannkuchreduxA(): void {
+        /* The Computer Language Benchmarks Game
+         * http://benchmarksgame.alioth.debian.org/
+         *
+         * contributed by Ledrug Katz
+         *
+         */
+
+        //#include < stdio.h >
+        //#include < stdlib.h >
+        //#include < stdint.h >
+
+        /* this depends highly on the platform.  It might be faster to use
+           char type on 32-bit systems; it might be faster to use unsigned. */
+
+        //int s[16], t[16];
+        var s = __Memory.AllocStack(16 * 4);
+        var t = __Memory.AllocStack(16 * 4);
+
+        var maxflips = 0;
+        var max_n;
+        var odd = 0;
+        var checksum = 0;
+
+        function flip(): number {
+            var i;
+            var x: Pointer, y: Pointer, c: number;
+
+            for (x = t, y = s, i = max_n; i--;) {
+                //* x++ = * y++;
+                x.SetInt32Value(y.GetInt32Value());
+                x = x.Add(4);
+                y = y.Add(4);
+            }
+            i = 1;
+            do {
+                for (x = t, y = t + t[0]; x < y;) {
+                    //c = * x, * x++ = * y, * y-- = c;
+                    c = x.GetInt32Value();
+                    x.SetInt32Value((y.GetInt32Value()));
+                    x = x.Add(4);
+                    y.SetInt32Value(c);
+                    y = y.Add(-4);
+                }
+                i++;
+            } while (t.GetInt32ValueOffset(t.GetInt32Value()) != 0/*t[t[0]]*/);
+            return i;
+        }
+
+        function rotate(n: number): void {
+            var c;
+            var i;
+            c = s.GetInt32Value();
+            for (i = 1; i <= n; i++) {
+                //s[i - 1] = s[i];
+                s.SetInt32ValueOffset(i - 1, s.GetInt32ValueOffset(i));
+            }
+            //s[n] = c;
+            s.SetInt32ValueOffset(n, c);
+        }
+
+        /* Tompkin-Paige iterative perm generation */
+        function tk(n: number): void {
+            var __stacktop = __Memory.GetStackTop();
+            var i = 0, f;
+            //int c[16] = { 0 };
+            var c = __Memory.AllocStack(4 * 16);
+
+            while (i < n) {
+                rotate(i);
+                if (c.GetInt16ValueOffset(i) >= i) {
+                    c.SetInt32ValueOffset(i++, 0);
+                    continue;
+                }
+
+                c.InclInt32ValueOffset(i);
+                i = 1;
+                odd = ~odd;
+                if (s.GetInt32Value() != 0) {
+                    f = (s.GetInt32ValueOffset(s.GetInt32Value()) != 0)/*s[s[0]]*/ ? flip() : 1;
+                    if (f > maxflips) {
+                        maxflips = f;
+                    }
+                    checksum += odd ? -f : f;
+                }
+            }
+            __Memory.SetStackTop(__stacktop);
+        }
+
+        function main(argc: number, v: Pointer): number {
+            var i;
+
+            if (argc < 2) {
+                printf("usage: %s number\n", v.GetPointerValue());
+                return 1;
+            }
+
+            max_n = atol(v.GetPointerValueOffset(8));
+            if (max_n < 3 || max_n > 15) {
+                printf("range: must be 3 <= n <= 12\n");
+                return 1;
+            }
+
+            for (i = 0; i < max_n; i++) {
+                s.SetInt32ValueOffset(i, i);
+            }
+            tk(max_n);
+
+            printf("%d\nPfannkuchen(%d) = %d\n", checksum, max_n, maxflips);
+
+            return 0;
+        }
+
+        var args = __Memory.AllocStack(16);
+
+        var num = __Memory.AllocStack(20);
+
+        num.SetCStringValueOffset(0, "5");
+        args.SetPointerValueOffset(8, num);
+
+        main(2, args);
+    }
+
+    //---------------------------------------------------------------------------------------
+    // spectralnorm.c
+    //---------------------------------------------------------------------------------------
+
+    spectralnormA(): void {
+        /* The Computer Language Benchmarks Game
+         * http://benchmarksgame.alioth.debian.org/
+         *
+         * Contributed by Mr Ledrug
+         *
+         * Algorithm lifted from Intel Fortran #2 code by Steve Decker et al.
+        */
+
+        //#include < stdio.h >
+        //#include < stdlib.h >
+        //#include < math.h >
+
+        function A(i: number, j: number): number {
+            return (((i + j) * (i + j + 1) / 2) | 0 + i + 1);
+        }
+
+        function dot(v: Pointer, u: Pointer, n: number): number {
+            var i;
+            var sum = 0;
+            for (i = 0; i < n; i++)
+                sum += v.GetFloat64ValueOffset(i) * u.GetFloat64ValueOffset(i);
+            return sum;
+        }
+
+        function mult_Av(v: Pointer, out: Pointer, n: number): void {
+            var i, j;
+            var sum;
+            for (i = 0; i < n; i++) {
+                for (sum = j = 0; j < n; j++) {
+                    sum += v.GetFloat64ValueOffset(j) / A(i, j);
+                }
+                out.SetFloat64ValueOffset(i, sum);
+            }
+        }
+
+        function mult_Atv(v: Pointer, out: Pointer, n: number): void {
+            var i, j;
+            var sum;
+            for (i = 0; i < n; i++) {
+                for (sum = j = 0; j < n; j++) {
+                    sum += v.GetFloat64ValueOffset(j) / A(j, i);
+                }
+                out.SetFloat64ValueOffset(i, sum);
+            }
+        }
+
+        var tmp: Pointer;
+
+        function mult_AtAv(v: Pointer, out: Pointer, n: number): void {
+            mult_Av(v, tmp, n);
+            mult_Atv(tmp, out, n);
+        }
+
+        function main(argc: number, argv: Pointer): number {
+            var n = atol(argv.GetPointerValueOffset(8));
+            if (n <= 0) n = 2000;
+
+            var u = __Memory.AllocHeap(n * 8);
+            var v = __Memory.AllocHeap(n * 8);
+            var tmp = __Memory.AllocHeap(n * 8);
+
+            var i;
+            for (i = 0; i < n; i++) {
+                u.SetFloat64ValueOffset(i, 1);
+            }
+            for (i = 0; i < 10; i++) {
+                mult_AtAv(u, v, n);
+                mult_AtAv(v, u, n);
+            }
+
+            printf("%.9f\n", Math.sqrt(dot(u, v, n) / dot(v, v, n)));
+
+            return 0;
+        }
+        var args = __Memory.AllocStack(16);
+
+        var num = __Memory.AllocStack(20);
+
+        num.SetCStringValueOffset(0, "2000");
+        args.SetPointerValueOffset(8, num);
+    }
+
     start() {
-        this.exexute("binarytreeA", this.binarytreeA);
+        this.exexute("spectralnormA", this.binarytreeA);
         //this.exexute("test1B", this.test1B);
     }
 
